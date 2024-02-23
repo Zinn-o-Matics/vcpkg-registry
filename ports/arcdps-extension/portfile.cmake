dc721b93bea14f12821cae1dbe59ffa08f161bcc
knox <knoxfighter@asdff45.de>
2024-02-23 12:36:09 +0100

    updated to optional dependency setup

--- ports/arcdps-extension/portfile.cmake
+++ ports/arcdps-extension/portfile.cmake
@@ -3,13 +3,21 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO knoxfighter/arcdps-extension
-    REF b713d9e990579065317a7c9cd76cccf179d36107
-    SHA512 243dbba5b106509a01cf2c647d985672df53ebbd5fc446c89b29ed793917f7b65e351024fefe38b4e1d634fdf18f3e759841c0225c1358252937b0bdf4e88e79
-    HEAD_REF vcpkg-update
+    REF ba1643658eafdd861371751a9d4c595f10867912
+    SHA512 05fb8b2ac62b7747ada6a23b9d5b72e351d6bc211fffe0d940a7023d80c37299864849a517b4ef2198529aa0f0195a2ff8d9e5472b31613f4157b8ffb528e245
+    HEAD_REF vcpkg-update2
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    imgui				ARCDPS_EXTENSION_IMGUI
+	unofficial-extras	ARCDPS_EXTENSION_UNOFFICIAL_EXTRAS
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+	OPTIONS
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
