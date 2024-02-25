350f649e159988d08c013c143109f723d240aeb9
knox <knoxfighter@asdff45.de>
2024-02-25 15:52:38 +0100

    updated nexus port

--- ports/nexus/portfile.cmake
+++ ports/nexus/portfile.cmake
@@ -1,9 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    #REPO RaidcoreGG/RCGG-lib-nexus-api
     REPO knoxfighter/RCGG-lib-nexus-api
     REF 1e5bd141ee3e1d5b50dd1aae1fb681584370d0ac
-    SHA512 cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e
+    SHA512 d023fbc8ea53b2b6c3bcca8a0d760cd7cf5e1e71615a6b3a4128918f5bc04cc3a95fe4816c4cfb710264e2543eb09ae6ecb8313c37fa9bfd8f5d5b98b00ef343
     HEAD_REF optional-imgui
 )
 
