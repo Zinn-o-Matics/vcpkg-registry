8b923f442e8a1f12fa6a3e79bf2ac2f1afe2e4b1
knox <knoxfighter@asdff45.de>
2024-02-25 15:50:05 +0100

    point nexus to fork

--- ports/nexus/portfile.cmake
+++ ports/nexus/portfile.cmake
@@ -1,9 +1,10 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO RaidcoreGG/RCGG-lib-nexus-api
-    REF a8b7fceab29a35da4ae0cc43f0c7f78fe2f8dc4f
-    SHA512 45d3cf07061095e6006b11063768493182734a8f2bf0a4742109a31b37894088ebce350eaab0aa00839bddb707fc25a66d578c473189928d72b5042b9b0e8537
-    HEAD_REF main
+    #REPO RaidcoreGG/RCGG-lib-nexus-api
+    REPO knoxfighter/RCGG-lib-nexus-api
+    REF 1e5bd141ee3e1d5b50dd1aae1fb681584370d0ac
+    SHA512 cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e
+    HEAD_REF optional-imgui
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
