0def071db07e3fd4a832c98ece40cb50ccbfa232
knox <knoxfighter@asdff45.de>
2024-03-03 17:11:46 +0100

    change nexus back to original repo and branch

--- ports/nexus/portfile.cmake
+++ ports/nexus/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO knoxfighter/RCGG-lib-nexus-api
-    REF 511eecdcdefb1699fde8b803f1b1634ba1cac08b
+    REPO RaidcoreGG/RCGG-lib-nexus-api
+    REF 8fd6bae6a1d40fd9089d30622b7406f8a1a93cb4
     SHA512 587693b4157ea8d0266d4f79d3f01f9496032766cba287303b38fab97e35da70230782941efe162ac7dc0ba5685131b94a8cb89b61bf1fdbccbd9b960a7b4b02
-    HEAD_REF optional-imgui
+    HEAD_REF main
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
