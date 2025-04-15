87c9988d761c8d9fcd482e2fc236340c897e5c08
knox <knoxfighter@asdff45.de>
2025-04-15 17:35:29 +0200

    updated arcdps-unofficial-extras port

--- ports/arcdps-unofficial-extras/portfile.cmake
+++ ports/arcdps-unofficial-extras/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Krappa322/arcdps_unofficial_extras_releases
-    REF ac7505651b96dfeb5848eb599fdfbf5de3d2823c
-    SHA512 f0065555834494d0672fff51534cca2d72ce0dc3d9cbe374071793e0a2ec5f4f856a16f4e84d14d3066d221bc3eb5e6ccb0d3de670066be10affbb3feed6fe3d
+    REF 077f17ed46b23cc9c8af6f7d0151f89f008c8787
+    SHA512 4e34160e172c63da6d5b13a6ff4d84482847c8242f7c11f5bf1d613e3ebd0a6d9e8a8d3b306e6686f79b5dfb5a4d6bcdcd340d8ba576a134c8dc8d695033c301
     HEAD_REF master
 )
 
