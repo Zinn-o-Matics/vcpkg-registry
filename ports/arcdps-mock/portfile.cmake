a5735e33d7639f44ff4fe512b2c2b79500118a2d
knox <knoxfighter@asdff45.de>
2025-07-24 19:37:31 +0200

    updated arcdps-mock port

---
+++ ports/arcdps-mock/portfile.cmake
@@ -0,0 +1,13 @@
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO Krappa322/arcdps_mock
+        REF 
+        SHA512 cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e
+        HEAD_REF vcpkg
+)
+
+# This is a header only library (for now)
+file(INSTALL "${SOURCE_PATH}/xevtc/Xevtc.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include/xevtc")
+
+# Handle copyright
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
