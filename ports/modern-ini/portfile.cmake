39400bc4e5520285bc55a5e7ad2b2efb3b1e9caf
knoxfighter <knoxfighter@asdff45.de>
2026-05-23 00:48:25 +0200

    updated modern-ini port

--- ports/modern-ini/portfile.cmake
+++ ports/modern-ini/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO knoxfighter/modernIni
-    REF 0
-    SHA512 0
+    REF fc24327426caf4a9dad610eced020376e2207978
+    SHA512 c012542bc70e56700e0622a40beccaa4381dd662241bc0ebc074cae32f90f3cd35d818ae9af8073bcc74cd716c5a27d533f4e6e59013e35c7927a387ee701c7e
     HEAD_REF main
 )
 
