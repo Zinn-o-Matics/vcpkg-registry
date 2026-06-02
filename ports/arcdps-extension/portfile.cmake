13fcdb57c9544f68af2aaecc6326561856905ed7
knox <knoxfighter@asdff45.de>
2026-06-02 22:34:05 +0200

    update arcdps-extension to org

--- ports/arcdps-extension/portfile.cmake
+++ ports/arcdps-extension/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO knoxfighter/arcdps-extension
+    REPO Zinn-o-Matics/arcdps-extension
     REF 6dbd01730e25f9981f631f466e59c61bfd9f725b
     SHA512 5e1425d1d9b09b681cd8fdf70624523997d638b0c30377929932d11f940dc651c7985ea839771a8f29722beccdf787faf817c75f0b61e863c210d029af920dc7
     HEAD_REF main
