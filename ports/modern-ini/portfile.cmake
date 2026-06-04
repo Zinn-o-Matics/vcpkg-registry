9eae5a57c003b255cd73aa2d1b623f32c9a28dc9
knox <knoxfighter@asdff45.de>
2026-06-04 15:47:55 +0200

    update modern-ini repo

--- ports/modern-ini/portfile.cmake
+++ ports/modern-ini/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO knoxfighter/modernIni
+    REPO Zinn-o-Matics/modernIni
     REF f461412775b3fec4ed167df56afd2e9eb6c423cf
     SHA512 54143fb9b5fe5763dc39e34e2bd803bda990de5c92d386515a70d0dc6af50c4f46029f1bc4ebb13192fda9f9e9df822a36c0db9b78ba086fb2f3b0a00eb4100d
     HEAD_REF main
